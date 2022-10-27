4020b4de252e42a9c9c5e4fd9e2f826d7c6e56e2
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-10-27 14:47:50 -0300

    #

---
+++ components/Mongagua.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+
+export default function Mongagua() {
+    return(
+    <View>
+       <Text> Seja Bem Vindo</Text>
+
+    <View>
+       <Text> Mongagua</Text>
+    </View>
+</View>
+);
+  }
